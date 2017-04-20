b5eef3e2f39f20fca4b140d2a48de86e009890b0
Nicholas Anderson <nranderson@users.noreply.github.com>
2017-04-19 22:57:01 -0400

    Update application.rb

--- config/settings/application.rb
+++ config/settings/application.rb
@@ -30,7 +30,7 @@ SimpleConfig.for :application do
     set :namespace, 'FACEBOOK_NAMESPACE'
     set :app_id, 'FACEBOOK_APP_ID'
     set :secret, 'FACEBOOK_SECRET'
-    set :scope, 'public_profile, publish_actions, user_birthday, user_about_me, user_email'
+    set :scope, 'public_profile, publish_actions, user_birthday, user_about_me'
     set :cache_expiry_time, 7.days
   end
 
